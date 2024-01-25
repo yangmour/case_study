45121edd1a3818f9bb58665834b283854b1fc449
xiwen <15133695630@163.com>
2024-01-25 09:43:09 +0800

    增加日志

--- java/groovy-demo/springboot-groovy-demo02/src/main/java/com/example/springbootgroovydemo02/controller/HelloController.groovy
+++ java/groovy-demo/springboot-groovy-demo02/src/main/java/com/example/springbootgroovydemo02/controller/HelloController.groovy
@@ -1,16 +1,18 @@
 package com.example.springbootgroovydemo02.controller
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CompileStatic
+@Slf4j
 class HelloController {
     @RequestMapping("/hello/groovy")
     def hello(String name) {
-        return "Hello " + name
+        def s = "Hello " + name
+        log.warn(s)
+        s
     }
 }
