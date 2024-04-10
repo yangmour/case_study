54038fd1167f44bed040fb44fdd2a9e8069ea75a
xiwen <15133695630@163.com>
2024-04-10 14:33:08 +0800

    能启动

---
+++ java/groovy-demo02/server-api/server-demo-api/src/main/java/com/xiwen/serverdemoapi/ServerDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.xiwen.serverdemoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerDemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerDemoApiApplication.class, args);
+    }
+
+}
---
+++ java/groovy-demo02/server-api/server-demo-api/src/test/java/com/xiwen/serverdemoapi/ServerDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiwen.serverdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDemoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
